647f964d7e456a13d628c636c0d2c76070ac680a
Botond Birta <birtabotond@gmail.com>
2024-02-23 14:44:52 +0200

    frontend initialization

---
+++ erasnap-web/src/app/artgen/artgen-page/artgen-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artgen-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './artgen-page.component.html',
+  styleUrl: './artgen-page.component.scss'
+})
+export class ArtgenPage {
+
+}
---
+++ erasnap-web/src/app/artgen/artgen.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtgenPage } from './artgen-page/artgen-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArtgenPage
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArtgenModule { }
