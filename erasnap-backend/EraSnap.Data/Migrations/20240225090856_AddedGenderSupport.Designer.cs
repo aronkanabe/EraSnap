dcbc09712462493368271ee2a772fcc4ac7be0d5
Aron Kanabe <aronkanabe@gmail.com>
2024-02-25 11:10:55 +0200

    Added Gender Support

--- erasnap-backend/EraSnap.Data/Migrations/20240225090856_AddedGenderSupport.Designer.cs
+++ erasnap-backend/EraSnap.Data/Migrations/20240225090856_AddedGenderSupport.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EraSnap.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EraSnap.Data.Migrations
 {
     [DbContext(typeof(EraSnapDbContext))]
-    partial class EraSnapDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240225090856_AddedGenderSupport")]
+    partial class AddedGenderSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
